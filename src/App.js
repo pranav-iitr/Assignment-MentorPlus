63734c7b2f97180ca228730cdcb9b5ef0718fcba
pranav <pranav_a@ece.iitr.ac.in>
2022-01-26 18:12:24 +0530

    main navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <head>
+        tttttttttt
+    </head>
      
     </div>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useState } from "react";
+import "../components/navbar.css";
+function Navbar() {
+  const [Workshop, setWorkshop] = useState(false);
+  const [Program, setProgram] = useState(false);
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="navbar">
+          <div className="navbar-items">
+            <div>
+              <pre> About </pre>
+            </div>
+          </div>
+
+          <div
+            className="navbar-items"
+            onMouseLeave={() => {
+              setWorkshop(false);
+            }}
+            onMouseEnter={() => {
+              setWorkshop(true);
+            }}
+          >
+            <div>
+              <pre> Workshop </pre>
+            </div>
+          </div>
+
+          <div className="navbar-items"
+            onMouseLeave={() => {
+              setProgram(false);
+            }}
+            onMouseEnter={() => {
+              setProgram(true);
+            }}
+          >
+            <div>
+              <pre> Programs </pre>
+            </div>
+          </div>
+
+          <div className="navbar-items">
+            <div>
+              <pre> Community </pre>
+            </div>
+          </div>
+
+          <div className="navbar-items">
+            <div>
+              <pre> Login </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-container">
+      <div className={Workshop ? "Workshops-active" : "Workshops-passive"}>
+        <div className="list-element"> Workshop 1</div>
+        <div className="list-element"> Workshop 2</div>
+        <div className="list-element"> Workshop 3</div>
+      </div>
+      </div>
+      <div className="flex-container">
+      <div className={Program ? "prog-active" : "prog-passive"}>
+        <div className="list-element"> prog 1</div>
+        <div className="list-element"> prog 2</div>
+        <div className="list-element"> prog 3</div>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
